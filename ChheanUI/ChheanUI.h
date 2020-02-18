6f3db42e139a230377f21d6716012e9b094cd2c5
SLC <slc@gmail.com>
2020-02-18 09:04:40 +0700

    Initial Commit

---
+++ ChheanUI/ChheanUI.h
@@ -0,0 +1,19 @@
+//
+//  ChheanUI.h
+//  ChheanUI
+//
+//  Created by andrewcls on 2/18/20.
+//  Copyright © 2020 sourleangchhean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChheanUI.
+FOUNDATION_EXPORT double ChheanUIVersionNumber;
+
+//! Project version string for ChheanUI.
+FOUNDATION_EXPORT const unsigned char ChheanUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChheanUI/PublicHeader.h>
+
+
